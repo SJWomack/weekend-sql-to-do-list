8797d45c83be7af7e09fb31cf50be0803659cc15
Stephen Womack <swomack111@gmail.com>
2022-05-28 17:59:49 -0500

    set up pool/routers

---
+++ server/modules/pool.js
@@ -0,0 +1,20 @@
+const pg = require('pg');
+
+
+const config ={
+    database: 'weekend-to-do-app',
+    host: 'localhost',
+    port: 5432
+}
+
+const pool = new pg.Pool(config)
+
+pool.on("connect", ()=>{
+    console.log('connected to postgres');
+});
+
+pool.on("error", (err)=>{
+    console.log('error connecting pg', err)
+});
+
+module.exports = pool;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const toDoRouter = require('./routes/toDo.router.js')
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use('/toDo', toDoRouter);
+
+
+
+
+
+
+
+app.use(express.static('server/public'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+    console.log('listening on port', PORT);
+});
